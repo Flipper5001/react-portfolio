f0fb7c281275974384854b482f2fa213573fb631
Flipper <filipe@motionfirestudios.com.au>
2022-10-03 18:47:29 +0800

    added helper files to store validation functions

---
+++ src/helpers/helpers.js
@@ -0,0 +1,9 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function validateForm(message) {
+    const re = /^[a-zA-Z0-9]{1,9999}$/;
+    return re.test(String(message).toLowerCase());
+}
