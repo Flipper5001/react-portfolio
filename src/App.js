236f851b8e5e1e65e9b6eb969c29170d6d26e421
Flipper <filipe@motionfirestudios.com.au>
2022-10-03 19:56:13 +0800

    adjusting code to attempt to fix error with loading on gh pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
@@ -6,30 +6,15 @@ import Resume from './pages/Resume';
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-    {
-      path: "/contact",
-      element: <Contact/>,
-    },
-    {
-      path: "/portfolio",
-      element: <Portfolio/>,
-    },
-    {
-      path: "/resume",
-      element: <Resume/>,
-    },
-  ]);
-
-
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/resume" element={<Resume/>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
